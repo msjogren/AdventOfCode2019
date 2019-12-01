c8cd022dcc1a4c117cc9e8d0fdea9fbb9b75fbb4
Mattias Sjögren <mattias.sjogren@jayway.com>
2019-12-01 22:05:33 +0100

    Solution for day 1

---
+++ day01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum1 = 0, sum2 = 0;
+
+        int FuelForMass(int mass) => (mass / 3) - 2;
+
+        foreach (string line in File.ReadAllLines("input.txt")) {
+            int fuel = FuelForMass(int.Parse(line)); 
+            sum1 += fuel;
+
+            while (fuel > 0) {
+                sum2 += fuel;
+                fuel = FuelForMass(fuel);
+            }
+        }
+
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+    }
+}
