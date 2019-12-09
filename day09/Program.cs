4a4b1feeb55234bcde15f61c8006e7a5617b6f35
Mattias Sjögren <mattias.sjogren@jayway.com>
2019-12-09 10:41:56 +0100

    Solution for day 9

---
+++ day09/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long[] code = File.ReadAllText("input.txt").Split(',').Select(long.Parse).ToArray();
+
+        Console.WriteLine("Part 1:");
+        long[] memory = new long[1_000_000];
+        Array.Copy(code, memory, code.Length);
+        RunProgram(memory, () => 1, i => {
+            Console.WriteLine("Output: " + i);
+        });
+
+        Console.WriteLine("Part 2:");
+        memory = new long[1_000_000];
+        Array.Copy(code, memory, code.Length);
+        RunProgram(memory, () => 2, i => {
+            Console.WriteLine("Output: " + i);
+        });
+    }
+
+    enum ParameterMode
+    {
+        Position,
+        Immediate,
+        Relative
+    }
+
+    static void RunProgram(long[] memory, Func<long> input, Action<long> output)
+    {
+        long ip = 0;
+        long baseAddress = 0;
+        bool halt = false;
+
+        ParameterMode ParamMode(int ipOffset)
+        {
+            int modeDigit = ipOffset + 2;
+            long mode = memory[ip];
+            while (--modeDigit > 0) mode /= 10;
+            return (ParameterMode)(mode %= 10);
+        }
+
+        long Read(int ipOffset) =>
+            ParamMode(ipOffset) switch {
+                ParameterMode.Position => memory[memory[ip + ipOffset]],
+                ParameterMode.Immediate => memory[ip + ipOffset],
+                ParameterMode.Relative => memory[baseAddress + memory[ip + ipOffset]],
+                _ => throw new InvalidProgramException()
+            };
+
+        void Write(int ipOffset, long value)
+        {
+            if (ParamMode(ipOffset) == ParameterMode.Relative) {
+                memory[baseAddress + memory[ip + ipOffset]] = value;
+            } else {
+                memory[memory[ip + ipOffset]] = value;
+            }
+        }
+
+        while (!halt) {
+            long opcode = memory[ip] % 100;
+            switch (opcode) {
+                case 1:
+                    Write(3, Read(1) + Read(2));
+                    ip += 4;
+                    break;
+                case 2:
+                    Write(3, Read(1) * Read(2));
+                    ip += 4;
+                    break;
+                case 3:
+                    Write(1, input());
+                    ip += 2;
+                    break;
+                case 4:
+                    var outVal = Read(1); 
+                    output(outVal);
+                    ip += 2;
+                    break;
+                case 5:
+                    if (Read(1) != 0) {
+                        ip = Read(2);
+                    } else {
+                        ip += 3;
+                    }
+                    break;
+                case 6:
+                    if (Read(1) == 0) {
+                        ip = Read(2);
+                    } else {
+                        ip += 3;
+                    }
+                    break;
+                case 7:
+                    Write(3, Read(1) < Read(2) ? 1 : 0);
+                    ip += 4;
+                    break;
+                case 8:
+                    Write(3, Read(1) == Read(2) ? 1 : 0);
+                    ip += 4;
+                    break;
+                case 9:
+                    baseAddress += Read(1);
+                    ip += 2;
+                    break;
+                case 99:
+                    halt = true;
+                    break;
+                default:
+                    Console.Error.WriteLine($"Invalid opcode {opcode} at address {ip}");
+                    halt = true;
+                    break;
+            }
+        }
+    }
+}
